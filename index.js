5a112ecaeb3334122826fd2a46aa0272bf847206
Jessica Davey <jessica@jessicadavey.com>
2019-04-16 12:31:29 -0500

    refactoring.

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-let currentTime = new Date();
-let currentHours = currentTime.getHours();
-let currentMinutes = currentTime.getMinutes();
+function tellTime() {
+  let currentTime = new Date();
+  let currentHours = currentTime.getHours();
+  let currentMinutes = currentTime.getMinutes();
 
-if (currentMinutes >= 33) currentHours++;
-document.querySelector(`._${currentHours}`).classList.add("active");
-
-if (currentMinutes >= 33 && currentMinutes < 57)
-  document.querySelector(`.to`).classList.add("active");
-
-if (currentMinutes >= 3 && currentMinutes < 33)
-  document.querySelector(`.past`).classList.add("active");
-
-if (currentMinutes >= 27 && currentMinutes < 33)
-  document.querySelector(`.half`).classList.add("active");
-
-if (
-  (currentMinutes >= 33 && currentMinutes < 37) ||
-  (currentMinutes >= 23 && currentMinutes < 27)
-) {
-  document.querySelector(`.twenty`).classList.add("active");
-  document.querySelector(`.five`).classList.add("active");
-  document.querySelector(`.minutes`).classList.add("active");
+  timeIntoWords(currentHours, currentMinutes);
 }
 
-if (
-  (currentMinutes >= 37 && currentMinutes < 43) ||
-  (currentMinutes >= 17 && currentMinutes < 23)
-) {
-  document.querySelector(`.twenty`).classList.add("active");
-  document.querySelector(`.minutes`).classList.add("active");
-}
+tellTime();
+setInterval(tellTime, 60000);
 
-if (
-  (currentMinutes >= 43 && currentMinutes < 47) ||
-  (currentMinutes >= 13 && currentMinutes < 17)
-)
-  document.querySelector(`.quarter`).classList.add("active");
+function timeIntoWords(hours, minutes) {
+  if (minutes >= 33) hours++;
+  document.querySelector(`._${hours}`).classList.add("active");
 
-if (
-  (currentMinutes >= 47 && currentMinutes < 53) ||
-  (currentMinutes >= 7 && currentMinutes < 13)
-) {
-  document.querySelector(`.ten`).classList.add("active");
-  document.querySelector(`.minutes`).classList.add("active");
-}
+  if (minutes >= 33 && minutes < 57)
+    document.querySelector(`.to`).classList.add("active");
 
-if (
-  (currentMinutes >= 53 && currentMinutes < 57) ||
-  (currentMinutes >= 3 && currentMinutes < 7)
-) {
-  document.querySelector(`.five`).classList.add("active");
-  document.querySelector(`.minutes`).classList.add("active");
+  if (minutes >= 3 && minutes < 33)
+    document.querySelector(`.past`).classList.add("active");
+
+  if (minutes >= 27 && minutes < 33)
+    document.querySelector(`.half`).classList.add("active");
+
+  if ((minutes >= 33 && minutes < 37) || (minutes >= 23 && minutes < 27)) {
+    document.querySelector(`.twenty`).classList.add("active");
+    document.querySelector(`.five`).classList.add("active");
+    document.querySelector(`.minutes`).classList.add("active");
+  }
+
+  if ((minutes >= 37 && minutes < 43) || (minutes >= 17 && minutes < 23)) {
+    document.querySelector(`.twenty`).classList.add("active");
+    document.querySelector(`.minutes`).classList.add("active");
+  }
+
+  if ((minutes >= 43 && minutes < 47) || (minutes >= 13 && minutes < 17))
+    document.querySelector(`.quarter`).classList.add("active");
+
+  if ((minutes >= 47 && minutes < 53) || (minutes >= 7 && minutes < 13)) {
+    document.querySelector(`.ten`).classList.add("active");
+    document.querySelector(`.minutes`).classList.add("active");
+  }
+
+  if ((minutes >= 53 && minutes < 57) || (minutes >= 3 && minutes < 7)) {
+    document.querySelector(`.five`).classList.add("active");
+    document.querySelector(`.minutes`).classList.add("active");
+  }
+  console.log(hours, minutes);
 }
